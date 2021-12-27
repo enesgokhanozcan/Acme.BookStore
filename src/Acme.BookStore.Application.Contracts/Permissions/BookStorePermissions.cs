083791b3c06e01682e5d74fd3ab1ad1715a79f46
Enes Gökhan Özcan <enes.gokhan.ozcan@gmail.com>
2021-12-27 20:51:11 +0300

    Authors: Application Layer

--- src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -11,5 +11,14 @@
             public const string Edit = Default + ".Edit";
             public const string Delete = Default + ".Delete";
         }
+
+        // *** ADDED a NEW NESTED CLASS ***
+        public static class Authors
+        {
+            public const string Default = GroupName + ".Authors";
+            public const string Create = Default + ".Create";
+            public const string Edit = Default + ".Edit";
+            public const string Delete = Default + ".Delete";
+        }
     }
 }
